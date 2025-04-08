73d5381daafe664de4efee4cc5d52d6a539f37f6
meta-prakshat-jain <prakshat.jain@metacube.com>
2025-04-08 15:16:09 +0530

    CountUniqueChar.java

--- CountUniqueChar.java
+++ CountUniqueChar.java
@@ -1,9 +1,20 @@
 package Assignment_4;
 
 import java.util.HashMap;
-
+import java.util.Scanner;
+/**
+ *  THis class helps to counts the number of unique characters in a
+ *   given string and stores the results in a HashMap.
+ */
 public class CountUniqueChar {
-	
+	/**
+	 * Method iterates through each character of the input string 
+	 * `str` using a for loop. This loop runs `n` times, 
+	 * where `n` is the length of the string.
+	 * @param str
+	 * @param uniqueString
+	 * @return the unique char in the given input
+	 */
 	static int uniqueCharacter (String str, HashMap<String, Integer> uniqueString) {
 		HashMap<Character, Integer> uniqueCharHashMap=new HashMap<>(); 
 		if(!uniqueString.containsKey(str)) {
@@ -12,17 +23,31 @@ public class CountUniqueChar {
 			uniqueCharHashMap.put(ch, 1);
 		}
 		uniqueString.put(str, uniqueCharHashMap.size());
+		}else {
+			System.out.println("Already Present in the Hash map retrive stored result");
 		}
-//		System.out.println(uniqueCharHashMap);
-		return uniqueString.size();
+		System.out.println(uniqueString);
+		System.out.println(uniqueCharHashMap);
+		
+		return uniqueString.get(str);
 	}
 	public static void main(String[] args) {
 		HashMap<String, Integer> uniqueString=new HashMap<>();
-		System.out.println(uniqueCharacter("World",uniqueString));
-		System.out.println(uniqueCharacter("Hello",uniqueString));
-		System.out.println(uniqueCharacter("World",uniqueString));
-		
-		System.out.println(uniqueString);
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Choose 1: to enter the string");
+		int choose=sc.nextInt();
+		sc.nextLine();
+		switch(choose) {
+		case 1:
+		while(true) {
+			System.out.println("Enter the String:");
+			String str=sc.nextLine();
+			System.out.println( uniqueCharacter(str, uniqueString));
+			if(str.equals("2"))break;
+		}
+		case 2:
+			break;
+		}
 		
 	}
 
